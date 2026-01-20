439b8cad40db46ccf55803fff89dad087b7c8779
allen <waverider0@users.noreply.github.com>
2026-01-20 17:52:05 -0500

    log2

--- log_approx.py
+++ log_approx.py
@@ -2,27 +2,23 @@
 
 # log10(2) = 0.3
 # log10(5) = 0.7
+# 1 / log10(2) = 3.3
 # 1 / log10(e) = 2.3
 
-import math
+from math import log, log2, log10
 import random
 
 if __name__ == '__main__':
-  problem_type = random.choice(['pow', 'log10', 'ln'])
+  f = random.choice(['pow', 'log10', 'log2', 'ln'])
 
-  if problem_type == 'pow':
-    a = random.randint(2, 20)
-    x = random.randint(1, 20)
+  if f == 'pow':
+    a,x = random.randint(2, 20), random.randint(1, 20)
     exact = a ** x
     ans = float(input(f'{a} ** {x} = '))
-  elif problem_type == 'log10':
-    x = random.randint(2, 500)
-    exact = math.log10(x)
-    ans = float(input(f'log10({x}) = '))
   else:
-    x = random.randint(2, 500)
-    exact = math.log(x)
-    ans = float(input(f'ln({x}) = '))
+    x = random.randint(2,500)
+    exact = (log if f=='ln' else log10 if f=='log10' else log2)(x)
+    ans = float(input(f"{f}({x}) = "))
 
   print(f'Exact: {round(exact, 2)}')
   print(f'Error: {round((ans - exact) / exact * 100, 2)}%')
