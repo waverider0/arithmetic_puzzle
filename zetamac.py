77780a4cd53c978037fb39dcc8bd5eeddef09877
allen <waverider0@users.noreply.github.com>
2026-01-26 13:00:26 -0500

    zetamac

--- zetamac.py
+++ zetamac.py
@@ -4,37 +4,43 @@ import random
 import threading
 import time
 
-OPS = ['Add', 'Sub', 'Mul', 'Div']
-ADD_L = [2, 1000]
-ADD_R = [2, 1000]
-MUL_L = [2, 100]
+OPS = ['+', '-', '*', '/']
+ADD_L = [2, 100]
+ADD_R = [2, 100]
+MUL_L = [2, 12]
 MUL_R = [2, 100]
 DURATION_SECONDS = 120
 
 if __name__ == '__main__':
   score = 0
   running = threading.Event()
-  timer = threading.Timer(DURATION_SECONDS, running.clear)
-
   running.set()
-  timer.start()
+  threading.Timer(DURATION_SECONDS, running.clear).start()
 
   while running.is_set():
     op = random.choice(OPS)
     match op:
-      case 'Add':
+      case '+' | '-':
         a = random.randint(*ADD_L)
         b = random.randint(*ADD_R)
-        correct = a + b
-        answer = int(input(f'{a} + {b} = '))
-      case 'Sub':
-        a = random.randint(*ADD_L)
-        b = random.randint(*ADD_R)
-        correct = a - b
-        answer = int(input(f'{a} - {b} = '))
-      case 'Mul':
-        pass
-      case 'Div':
-        pass
-
-  print(score)
+        exact = a+b if op == '+' else a-b
+        ans = None
+        while ans != exact: ans = int(input(f'{a} {op} {b} = ')) # FIXME: a non-number typo breaks the program and ends the game prematurely
+        score += 1
+        print(f'Score: {score}')
+      case '*':
+        a = random.randint(*MUL_L)
+        b = random.randint(*MUL_R)
+        exact = a * b
+        ans = None
+        while ans != exact: ans = int(input(f'{a} * {b} = '))
+        score += 1
+        print(f'Score: {score}')
+      case '/':
+        a = random.randint(*MUL_L)
+        b = random.randint(*MUL_R)
+        exact = a / b
+        ans = None
+        while ans is None or abs((ans - exact) / exact > 0.01): ans = float(input(f'{a} / {b} = '))
+        score += 1
+        print(f'Score: {score}')
